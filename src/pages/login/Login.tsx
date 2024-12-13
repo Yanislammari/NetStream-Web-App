4cc8781082b4758145b54dadd03c42968879746a
yanislammari <yanis.lammari937@gmail.com>
2024-12-13 11:36:09 +0100

    add login page ui

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <div className="Login">
-      <div className="hero-banner">
-        <div className="hero-banner-top">
-          <img src="./assets/netstream-logo.png" onClick={() => navigate("/")}></img>
+      <div className="hero-banner-login">
+        <div className="hero-banner-top-login">
+          <img src="./assets/netstream-logo.png" alt="Logo" onClick={() => navigate("/")} />
         </div>
-        <div className="hero-banner-content">
-          {/* Formulaire */}
+        <div className="form-container">
+          <h2>Sign In</h2>
+          <form>
+            <div className="form-group">
+              <input type="email" id="email" placeholder="Email address" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Please enter a valid email address !" required />
+              <input type="password" id="password" placeholder="Password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" title="Password must contain at least 8 characters, including an uppercase letter, a lowercase letter, a number, and a special character !" required />
+            </div>
+            <p className="forgot-password">Forgot your password ?</p>
+            <button type="submit" className="submit-btn">Sign In</button>
+          </form>
+          <p className="redirect-text">Don't have an account ? <span onClick={() => navigate("/register")} className="redirect-link">Sign up here !</span></p>
         </div>
       </div>
     </div>
