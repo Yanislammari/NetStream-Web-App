5e7103b6f486e02a037d74a7c1dbc1f7f8479298
yanislammari <yanis.lammari937@gmail.com>
2024-12-30 20:39:02 +0100

    add fetching medias from backend in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,11 +8,38 @@ import { toast } from "sonner";
 import "./Home.css";
 
 const Home: React.FC = () => {
+  const [medias, setMedias] = useState<Media[]>([]);
+
+  useEffect(() => {
+    const fetchMedias = async () => {
+      try {
+        const fetchedMedias = await getAllMedias();
+        if(fetchedMedias === "Internal servor error !") {
+          toast.error("Internal servor error !");
+          return;
+        }
+        else {
+          setMedias(fetchedMedias as Media[]);
+        }
+      }
+      catch(err) {
+        toast.error("Internal servor error !");
+        return;
+      }
+    }
+    
+    fetchMedias();
+  }, []);
+
   return (
     <div className="Home">
       <Navbar />
-      <HeroBanner backgroundImage="" title="" subtitle="" />
-      <MediaSlideshow title="Last medias" medias={[]} />
+      <HeroBanner backgroundImage={medias[2]?.largePicture ?? ""} title={medias[0]?.name} subtitle={medias[0]?.synopsis} />
+      <MediaSlideshow title="Last medias" medias={medias} />
+      <MediaSlideshow title="Last medias" medias={medias} />
+      <MediaSlideshow title="Last medias" medias={medias} />
+      <MediaSlideshow title="Last medias" medias={medias} />
+      <MediaSlideshow title="Last medias" medias={medias} />
     </div>
   );
 };
