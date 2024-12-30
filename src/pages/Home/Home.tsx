c7c26ff6138edc0bd6e6a8db9601d867af07e992
yanislammari <yanis.lammari937@gmail.com>
2024-12-30 21:08:20 +0100

    add fetching movies from backend in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import Navbar from "../../components/Navbar/Navbar";
 import HeroBanner from "../../components/HeroBanner/HeroBanner";
 import MediaSlideshow from "../../components/MediaSlideshow/MediaSlideshow";
 import Media from "../../models/Media";
-import { getAllMedias } from "../../services/MediaService";
+import MediaType from "../../models/MediaType";
+import { getAllMedias, getAllMediasByMediaType } from "../../services/MediaService";
 import { toast } from "sonner";
 import "./Home.css";
 
 const Home: React.FC = () => {
   const [medias, setMedias] = useState<Media[]>([]);
+  const [movies, setMovies] = useState<Media[]>([]);
 
   useEffect(() => {
     const fetchMedias = async () => {
@@ -26,20 +28,41 @@ const Home: React.FC = () => {
         toast.error("Internal servor error !");
         return;
       }
-    }
+    };
+
+    const fetchMediasMovies = async () => {
+      try {
+        const fetchedMovies = await getAllMediasByMediaType(MediaType.Movie);
+        switch(fetchedMovies) {
+          case "Invalid MediaType !": {
+            toast.error("Error during fetching movies !");
+            return;
+          }
+          case "Internal servor error !": {
+            toast.error("Internal servor error !");
+            return;
+          }
+          default: {
+            setMovies(fetchedMovies as Media[]);
+          }
+        }
+      }
+      catch(err) {
+        toast.error("Internal servor error !");
+        return;
+      }
+    };
     
     fetchMedias();
+    fetchMediasMovies();
   }, []);
 
   return (
     <div className="Home">
       <Navbar />
       <HeroBanner backgroundImage={medias[2]?.largePicture ?? ""} title={medias[0]?.name} subtitle={medias[0]?.synopsis} />
-      <MediaSlideshow title="Last medias" medias={medias} />
-      <MediaSlideshow title="Last medias" medias={medias} />
-      <MediaSlideshow title="Last medias" medias={medias} />
-      <MediaSlideshow title="Last medias" medias={medias} />
-      <MediaSlideshow title="Last medias" medias={medias} />
+      <MediaSlideshow title="Latest Releases" medias={medias} />
+      <MediaSlideshow title="Latest Movies" medias={movies} />
     </div>
   );
 };
