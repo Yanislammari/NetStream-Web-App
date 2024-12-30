a85f82acc48d101c11a25c5a99b3145ff267ed19
yanislammari <yanis.lammari937@gmail.com>
2024-12-30 20:52:28 +0100

    media service: add get all medias by media type

--- src/services/MediaService.ts
+++ src/services/MediaService.ts
@@ -1,5 +1,6 @@
 import { BASE_URL } from "../config/API";
 import Media from "../models/Media";
+import MediaType from "../models/MediaType";
 
 export async function getAllMedias(): Promise<Media[] | string> {
   try {
@@ -11,6 +12,50 @@ export async function getAllMedias(): Promise<Media[] | string> {
     });
 
     if(response.status === 500) {
+      return "Internal servor error !";
+    }
+
+    const medias: Media[] = await response.json();
+    return medias;
+  }
+  catch(err) {
+    return "Internal servor error !";
+  }
+}
+
+export async function getAllMediasByMediaType(mediaType: MediaType): Promise<Media[] | string> {
+  try {
+    let mediaTypeParams = "";
+    
+    switch(mediaType) {
+      case MediaType.Movie: {
+        mediaTypeParams = "movies";
+        break;
+      }
+      case MediaType.Series: {
+        mediaTypeParams = "series";
+        break;
+      }
+      case MediaType.Episode: {
+        mediaTypeParams = "Episode";
+        break;
+      }
+      default: {
+        return "Invalid MediaType !";
+      }
+    }
+
+    const response = await fetch(`${BASE_URL}/medias/${mediaTypeParams}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }, 
+    });
+
+    if(response.status === 404) {
+      return "Invalid MediaType !";
+    }
+    else if(response.status === 500) {
       return "Internal servor error !";
     }
 
